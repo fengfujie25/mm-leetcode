806a8a7b42fbc02269d262ffdbf64e17e6e8b695
fujie feng <fujie.feng@qunar.com>
2020-04-12 15:12:20 +0800

    给你两个二进制字符串，返回它们的和（用二进制表示）

---
+++ src/main/java/com/maomao/leetcode/solution/solution/AddBinary.java
@@ -0,0 +1,56 @@
+package com.maomao.leetcode.solution.solution;
+
+/**
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ *
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ *
+ * 示例 1:
+ *
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ * 示例 2:
+ *
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ *  
+ *
+ * 提示：
+ *
+ * 每个字符串仅由字符 '0' 或 '1' 组成。
+ * 1 <= a.length, b.length <= 10^4
+ * 字符串如果不是 "0" ，就都不含前导零。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-binary
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author fujie.feng
+ * @Date 2020-04-12
+ */
+public class AddBinary {
+
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int ca = 0;
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
+            int sum = ca;
+            sum += i >= 0 ? a.charAt(i) - '0' : 0;
+            sum += j >= 0 ? b.charAt(j) - '0' : 0;
+            sb.append(sum % 2);
+            ca = sum / 2;
+        }
+        sb.append(ca == 1 ?  ca : "");
+        return sb.reverse().toString();
+    }
+
+    public String addBinary2(String a, String b) {
+        return Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
+    }
+
+    public static void main(String[] args) {
+        AddBinary addBinary = new AddBinary();
+        System.out.println(addBinary.addBinary2("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+                "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
+        ));
+    }
+}
